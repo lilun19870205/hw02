e52a185f570082156cf8a456febff9ede5846077
lilun19870205 <lilun@ksu.edu>
2015-09-29 14:03:07 -0500

    new added

--- Li_Lun_hw2p2.py
+++ Li_Lun_hw2p2.py
@@ -6,6 +6,8 @@ Created on Mon Sep 28 09:26:20 2015
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy as sp
+import scipy.integrate
 
 N=np.linspace(1,10,10)
 
@@ -23,15 +25,32 @@ I3_gauss=np.linspace(0,0,10)
 
 abs_error_I1_middlepoint=np.linspace(0,0,10)
 
-a=0
+
+#for i in xrange(1,11):
+##    I1_middlepoint[i-1] = 0
+#    for j in xrange(1,i+1):
+#        k=j+0.0
+#        m=i+0.0
+#        I1_middlepoint[i-1]=I1_middlepoint[i-1]+np.power((-1.+(k-0.5)*(2/m)),5);
+#        
+#    abs_error_I1_middlepoint[i-1]=I1_middlepoint[i-1]-0
+#    
+#plt.plot(N,np.log10(abs_error_I1_middlepoint),'r--')
+
+
 for i in xrange(1,11):
-#    I1_middlepoint[i-1] = 0
-    for j in xrange(1,i+1):
-        k=j+0.0
-        m=i+0.0
-        I1_middlepoint[i-1]=I1_middlepoint[i-1]+np.power((-1.+(k-0.5)*(2/m)),5);
-        
-    abs_error_I1_middlepoint[i-1]=I1_middlepoint[i-1]-0
+    f=lambda x:x**5
+    I1_quad=sp.integrate.quad(f,-1,1)
+    I1_middlepoint[i-1]=I1_quad[0]
+    I1_x=np.linspace(-1,1,i)
+    I1_y=I1_x**5
+    I1_simpson[i-1]=sp.integrate.simps(I1_y,I1_x)
+    a=np.polynomial.legendre.leggauss(i)
+    xi=-1+(2/2)*(1+a[0])
+    for j in xrange(0,i):
+        I1_gauss[i-1]=I1_gauss[i-1]+(xi[j]**5)*a[1][j]
     
-plt.plot(N,np.log(abs_error_I1_middlepoint[,10]))
-np.log()
+I1_middlepoint_error=I1_middlepoint-0.
+I1_simpson_error=I1_simpson - 0.
+I1_gauss_error=I1_gauss-0.
+
