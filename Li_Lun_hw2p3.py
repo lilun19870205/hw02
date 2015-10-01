74fffa224e6be1c1d5e17724dfd048284d966dcd
lilun19870205 <lilun@ksu.edu>
2015-10-01 09:44:57 -0500

    p3 added

--- Li_Lun_hw2p3.py
+++ Li_Lun_hw2p3.py
@@ -4,10 +4,12 @@ Created on Wed Sep 30 10:18:14 2015
 
 @author: lilun
 """
+import time
 
 
 n=input('please input number n :')
 i=2
+t0=time.time()
 while(i<=n):
     j=2
     while(j<=(i/j)):
@@ -17,3 +19,6 @@ while(i<=n):
     i=i+1
 
 print "all prime number below n is found"
+te=time.time()
+t=te-t0
+print t
