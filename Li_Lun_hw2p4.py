f92b08b81ae3b8d208a490569922f06032870db0
lilun19870205 <lilun@ksu.edu>
2015-10-01 15:12:44 -0500

    p4 added

--- Li_Lun_hw2p4.py
+++ Li_Lun_hw2p4.py
@@ -42,11 +42,29 @@ def decimal_to_binary(x,n):
     
     return s1,s2
 
+def binary_to_decimal(i,f):
+    m=len(i)
+    n=len(f)
+    integer_part=0.
+    float_part=0.
+    for j in xrange(0,m):
+        integer_part=integer_part + float(i[j])*2**(m-j-1)
+    for j in xrange(0,n):
+        float_part=float_part + float(f[j])*(0.5)**(j+1)
+    return integer_part+float_part
+        
+
 number=input()
 precision=input()
 #integer_part,float_part=decimal_to_binary(number,precision)
 result=decimal_to_binary(number,precision)
 print "the binary type of number",number,"is:",result[0],".",result[1]
 
+print " please input the integer part of binary number:"
+integer_part=raw_input()
+print " please input the float part of binary number:"
+float_part=raw_input()
+result=binary_to_decimal(integer_part,float_part)
+print "the decimal type of the number",integer_part,".",float_part,"is:",result
 
             
