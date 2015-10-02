3f04afe01aa71c4ccde1dbb18ada516e02fe127a
lilun19870205 <lilun@ksu.edu>
2015-10-02 08:59:45 -0500

    p3 added

--- Li_Lun_hw2p3.py
+++ Li_Lun_hw2p3.py
@@ -7,17 +7,27 @@ Created on Wed Sep 30 10:18:14 2015
 import time
 
 
-n=input('please input number n :')
-i=2
-t0=time.time()
-while(i<=n):
-    j=2
-    while(j<=(i/j)):
-        if not(i%j):break
-        j=j+1
-    if(j>i/j): print i, " is prime"
-    i=i+1
 
+a=[]
+
+
+def find_primes(n):
+    i=2
+    while(i<=n):
+        j=2
+        while(j<=(i/j)):
+             if not(i%j):break
+             j=j+1
+        if(j>i/j): 
+            print i, " is prime"
+            a.append(i)
+        i=i+1
+    return a
+
+n=input('please input number n :')
+t0=time.time()
+b=find_primes(n)
+print b
 print "all prime number below n is found"
 te=time.time()
 t=te-t0
